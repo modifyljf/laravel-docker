e73175a7a8ebb8fc8b67aa7476807d5589994e66
Jianfeng Li <modifyljf@gmail.com>
2022-11-12 01:21:22 -0500

    init: First Commit

--- app.sh
+++ app.sh
@@ -2,14 +2,14 @@
 
 if [ "$1" == "stop" ]; then
   docker compose \
-    -f ./app/docker-compose.yml \
+    -f docker-compose.yml \
     -v \
     --env-file .env \
     --project-name app \
     down
 else
   docker compose \
-    -f ./app/docker-compose.yml \
+    -f docker-compose.yml \
     --env-file .env \
     --project-name app \
     up
