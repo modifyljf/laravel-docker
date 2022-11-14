b39886265e0f8e9760b2ab14bab10b125395c80d
Jianfeng Li <modifyljf@gmail.com>
2022-11-14 10:48:56 -0500

    first

--- app.sh
+++ app.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
-if [ "$1" == "stop" ]; then
+function up() {
+  docker compose \
+    -f docker-compose.yml \
+    --env-file .env \
+    --project-name app \
+    up \
+    -d
+}
+
+function down() {
   docker compose \
     -f docker-compose.yml \
     -v \
     --env-file .env \
     --project-name app \
     down
+}
+
+if [ "$1" == "stop" ]; then
+  down
 else
-  docker compose \
-    -f docker-compose.yml \
-    --env-file .env \
-    --project-name app \
-    up
+  up
 fi
